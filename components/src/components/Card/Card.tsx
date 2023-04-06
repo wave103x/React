9c0a17b4d53ee889c16c08a1308632686e864a66
Mikhail Sprygin <wave103x@gmail.com>
2023-04-06 21:06:12 +0300

    feat: add modal window

--- components/src/components/Card/Card.tsx
+++ components/src/components/Card/Card.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { createPortal } from 'react-dom';
 
 import { CardModal } from '../CardModal/CardModal';
@@ -18,7 +18,11 @@ export const Card = ({ product }: { product: IProduct }) => {
       <p className={styles.title}>{product.title}</p>
       <p className={styles.category}>{product.category}</p>
       <p className={styles.price}>{product.price}</p>
-      {showModal && createPortal(<CardModal onClose={() => setShowModal(false)} />, document.body)}
+      {showModal &&
+        createPortal(
+          <CardModal product={product} onClose={() => setShowModal(false)} />,
+          document.body
+        )}
     </div>
   );
 };
--- components/src/components/CardModal/CardModal.tsx
+++ components/src/components/CardModal/CardModal.tsx
@@ -1,12 +1,27 @@
-import React, { useEffect } from 'react';
+import React from 'react';
+
+import IProduct from '../../Types/Product';
 
 import styles from './CardModal.module.scss';
 
-export const CardModal = ({ onClose }: { onClose: () => void }) => {
+export const CardModal = ({ onClose, product }: { onClose: () => void; product: IProduct }) => {
+  const bgClickHandler = (event: React.MouseEvent) => {
+    if (event.target === event.currentTarget) onClose();
+  };
   return (
-    <div className={styles.modalBg}>
-      CardModal
-      <button onClick={onClose}>close</button>
+    <div className={styles.modalBg} onClick={(event: React.MouseEvent) => bgClickHandler(event)}>
+      <div className={styles.modal}>
+        <div className={styles.imageWrapper}>
+          <img src={product.thumbnail} alt="product image" />
+        </div>
+        <p className={styles.title}>{product.title}</p>
+        <p className={styles.category}>{product.category}</p>
+        <p className={styles.description}>{product.description}</p>
+        <p className={styles.price}>{product.price}</p>
+        <button className={styles.close} onClick={onClose}>
+          🗙
+        </button>
+      </div>
     </div>
   );
 };
